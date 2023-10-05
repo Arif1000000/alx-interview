5d182572f1440675316fcbdccf42ef0c61790601
issah <muhammed.arif11@gmail.com>
2023-10-05 16:02:45 -0700

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,24 @@
-#!/usr/bin/python3 
- """Script will unlock list of lists""" 
-  
-  
- def canUnlockAll(boxes): 
-     """This function will take a list of lists and the content 
-        of a list will unlock other lists 
-     """ 
-  
-     keys = [0] 
-     for key in keys: 
-         for boxKey in boxes[key]: 
-             if boxKey not in keys and boxKey < len(boxes): 
-                 keys.append(boxKey) 
-     if len(keys) == len(boxes): 
-         return True 
-     return False
+#!/usr/bin/python3
+
+from collections import deque
+
+def canUnlockAll(boxes):
+    # Create a list to track visited boxes; initialize all as False
+    visited = [False] * len(boxes)
+    # Create a queue to store boxes to be checked, start with box 0
+    queue = deque([0])
+    # Mark the first box as visited
+    visited[0] = True
+
+    # Start traversal
+    while queue:
+        current_box = queue.popleft()  # Take the first box from the queue
+        # Check keys in the current box
+        for key in boxes[current_box]:
+            if not visited[key]:  # If the key can open an unvisited box
+                visited[key] = True  # Mark the box as visited
+                queue.append(key)  # Add the box to the queue for further checking
+
+    # Check if all boxes have been visited
+    return all(visited)
+
