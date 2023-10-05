3af3f2cbec6653192e923ebaced2cc1651f11dd8
issah <muhammed.arif11@gmail.com>
2023-10-05 16:26:50 -0700

    Deternines if all the boxes can be opened

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
+"""Script will unlock list of lists"""
 
-from collections import deque
 
 def canUnlockAll(boxes):
-    # Create a list to track visited boxes; initialize all as False
-    visited = [False] * len(boxes)
-    # Create a queue to store boxes to be checked, start with box 0
-    queue = deque([0])
-    # Mark the first box as visited
-    visited[0] = True
+    """This function will take a list of lists and the content
+       of a list will unlock other lists
+    """
 
-    # Start traversal
-    while queue:
-        current_box = queue.popleft()  # Take the first box from the queue
-        # Check keys in the current box
-        for key in boxes[current_box]:
-            if not visited[key]:  # If the key can open an unvisited box
-                visited[key] = True  # Mark the box as visited
-                queue.append(key)  # Add the box to the queue for further checking
-
-    # Check if all boxes have been visited
-    return all(visited)
-
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
